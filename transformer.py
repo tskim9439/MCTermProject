eccdca644b6dd90296665a91594f7b459e2dd6bb
Kang-Honggu <150495928+Kang-Honggu@users.noreply.github.com>
2023-12-04 17:44:15 +0900

    Update transformer.py

--- transformer.py
+++ transformer.py
@@ -1,42 +1,37 @@
 import torch
 import torch.nn as nn
+import math
+
 
 class Transformer(nn.Module):
-    def __init__(self, opt, num_classes, d_model=256, nhead=4, num_encoder_layers=3, num_decoder_layers=3, dropout=0.1):
+    def __init__(self, opt, num_classes, d_model=256, nhead=4, num_encoder_layers=3, dropout=0.1, max_len=5000):
         super(Transformer, self).__init__()
 
-        # Parameters similar to GRU and LSTM models
         self.d_model = d_model
-        self.nhead = nhead
-        self.num_encoder_layers = num_encoder_layers
-        self.num_decoder_layers = num_decoder_layers
-        self.dropout = dropout
 
-        # Embedding layer
         self.embedding = nn.Embedding(num_classes, d_model)
-
-        # Transformer specific layers
-        self.transformer = nn.Transformer(d_model, nhead, num_encoder_layers, num_decoder_layers, dropout=dropout)
-
-        # Decoder to map Transformer output to class labels
+        self.positional_encoding = self._generate_positional_encoding(max_len, d_model)
+        self.encoder = nn.TransformerEncoder(nn.TransformerEncoderLayer(d_model, nhead, dropout=dropout), num_encoder_layers)
         self.decoder = nn.Linear(d_model, num_classes)
 
         self._init_weights()
 
     def _init_weights(self):
-        # Initialize weights
         nn.init.xavier_uniform_(self.embedding.weight)
         nn.init.xavier_uniform_(self.decoder.weight)
 
-    def forward(self, src, tgt):
-        # Assuming src and tgt are provided with proper padding and batch dimensions
-        src = self.embedding(src) * torch.sqrt(torch.tensor(self.d_model, dtype=torch.float32))
-        tgt = self.embedding(tgt) * torch.sqrt(torch.tensor(self.d_model, dtype=torch.float32))
+    def _generate_positional_encoding(self, max_len, d_model):
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        return nn.Parameter(pe, requires_grad=False)
 
-        # Forward pass through the Transformer
-        output = self.transformer(src, tgt)
-
-        # Pass through the decoder
-        output = self.decoder(output)
-
+    def forward(self, src):
+        src = self.embedding(src) * math.sqrt(self.d_model)
+        src = src + self.positional_encoding[:src.size(0), :]
+        output = self.encoder(src)
+        output = self.decoder(output[-1])  # Assuming prediction based on the last state
         return output
