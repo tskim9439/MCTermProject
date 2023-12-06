a547ecaa665579a1a078f3d43f7270dd1c620028
Kang-Honggu <150495928+Kang-Honggu@users.noreply.github.com>
2023-12-06 10:10:34 +0900

    models-init.py

---
+++ __init__.py
@@ -0,0 +1,30 @@
+from .lstm import LSTM
+from .gru import GRU
+from .srgnn import SessionGraph
+from .transformer import transformer
+
+
+def get_model(cfg, num_classes):
+    name = cfg.model.name
+    if name == "lstm":
+        return LSTM(
+            opt=cfg.model.lstm,
+            num_classes=num_classes,
+        )
+    elif name == "gru":
+        return GRU(
+            opt=cfg.model.gru,
+            num_classes=num_classes,
+        )
+    elif name == "transformer":
+        return transformer(
+            opt=cfg.model.gru,
+            num_classes=num_classes,
+        )
+    elif name == "srgnn":
+        return SessionGraph(
+            opt=cfg.model.srgnn,
+            n_node=num_classes,
+        )
+    else:
+        raise ValueError(f"Model {name} not supported.")
