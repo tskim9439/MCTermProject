d83b77d577149f746de2e1d6c4bc4ededb85b757
Kang-Honggu <150495928+Kang-Honggu@users.noreply.github.com>
2023-12-09 18:19:42 +0900

    1209 1815

---
+++ data_pangyo.py
@@ -0,0 +1,181 @@
+import csv
+import random
+import numpy as np
+import pickle
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+from sklearn.model_selection import train_test_split
+
+import os
+
+def get_sequences_from_csv(csv_file_path):
+    sequences = []
+    with open(csv_file_path, "r") as f:
+        reader = csv.reader(f)
+        for i, data in enumerate(reader):
+            if i % 2 == 0:
+                sequence = data[1:]
+                sequences.append(sequence)
+    return sequences
+
+def get_sequences_from_directory(directory_path):
+    all_sequences = []
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(directory_path, filename)
+            sequences = get_sequences_from_csv(file_path)
+            all_sequences.extend(sequences)
+    return all_sequences
+
+def struct_dataset(sequences):
+    data = []
+    for line in sequences:
+        seq = []
+        for node in line:
+            if len(seq) == 0:
+                seq.append(node)
+                continue
+            if node != seq[-1]:
+                seq.append(node)
+        data.append(seq)
+    return data
+
+def filter_dataset(sequences, target_len=5):
+    data = []
+    for line in sequences:
+        if len(line) >= target_len:
+            #start_idx = random.randint(0, len(line) - target_len)
+            #data.append(line[start_idx:start_idx + target_len])
+            data.append(line)
+    return data
+
+def get_total_nodes(sequences):
+    nodes = set()
+    for line in sequences:
+        for node in line:
+            nodes.add(node)
+    return list(nodes)
+
+def data_masks(all_usr_pois, item_tail):
+    us_lens = [len(upois) for upois in all_usr_pois]
+    len_max = max(us_lens)
+    us_pois = [upois + item_tail * (len_max - le) for upois, le in zip(all_usr_pois, us_lens)]
+    us_msks = [[1] * le + [0] * (len_max - le) for le in us_lens]
+    return us_pois, us_msks, len_max
+
+class PangyoDataset(Dataset):
+    def __init__(self, data, node_info, seq_len=8):
+        self.data = data
+        self.node_info = node_info
+        self.seq_len = seq_len
+        self.nodes2idx = self.node_info["node2idx"]
+    
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        line = self.data[idx]
+        line = [self.nodes2idx[node] for node in line]
+        
+        # 랜덤하게 시작 인덱스 선택
+        start_idx = random.randint(0, len(line) - self.seq_len)
+        line = line[start_idx:start_idx + self.seq_len]
+        #line = line[:self.seq_len]
+        
+        session = np.array(line[:-1])
+        session = torch.from_numpy(session).long()
+        label = np.array(line[-1])
+        label = torch.from_numpy(label).long()
+        
+        inputs = torch.zeros(self.seq_len).long()
+        inputs[:len(session)] = session
+        mask = torch.zeros(self.seq_len).bool()
+        mask[:len(session)] = True
+        return inputs, label, mask
+
+def collate_fn(batch):
+    inputs, targets, mask = zip(*batch)
+    inputs = torch.stack(inputs)
+    mask = torch.stack(mask)
+    targets = torch.stack(targets)
+   
+    items, n_node, A, alias_inputs = [], [], [], []
+    # Items : List of items in each session with ascending order
+    # n_node : Number of unique items in each session with zero
+    # A : Adjacency matrix of each session
+    # alias_inputs : List of indices of items in each session
+    for u_input in inputs:
+        n_node.append(len(np.unique(u_input)))
+    max_n_node = max(n_node) # Most number of nodes in a graph
+
+    for u_input in inputs:
+        u_input = u_input.numpy()
+        node = np.unique(u_input)
+        items.append(node.tolist() + (max_n_node - len(node)) * [0])
+        u_A = np.zeros((max_n_node, max_n_node))
+        for i in np.arange(len(u_input) - 1):
+            if u_input[i + 1] == 0:
+                break
+            u = np.where(node == u_input[i])[0][0]
+            v = np.where(node == u_input[i + 1])[0][0]
+            u_A[u][v] = 1
+        u_sum_in = np.sum(u_A, 0)
+        u_sum_in[np.where(u_sum_in == 0)] = 1
+        u_A_in = np.divide(u_A, u_sum_in)
+        u_sum_out = np.sum(u_A, 1)
+        u_sum_out[np.where(u_sum_out == 0)] = 1
+        u_A_out = np.divide(u_A.transpose(), u_sum_out)
+        u_A = np.concatenate([u_A_in, u_A_out]).transpose()
+        A.append(u_A)
+        alias_inputs.append([np.where(node == i)[0][0] for i in u_input])
+
+    items = torch.Tensor(items).long()
+    A = torch.Tensor(A).float()
+    alias_inputs = torch.Tensor(alias_inputs).long()
+    n_node = torch.Tensor(n_node).long()
+    
+    return alias_inputs, A, items, mask, targets, inputs
+
+#시드 값 조정을 통해 다른 환경에서도 동일하게 shuffle된 데이터 사용
+def set_seed(seed_value = 1209):
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+
+def get_total_nodes(sequences):
+    nodes = set()
+    for line in sequences:
+        nodes.update(line)
+    return list(nodes)
+   
+
+if __name__ == "__main__":
+    # Setting the seed
+    set_seed()
+    directory_path = 'C:/Users/HONGGU/.conda/MCterm/datasets/00.processed_csv_file_ssp/5sequence/'
+    all_sequences = get_sequences_from_directory(directory_path)
+    # print(all_sequences)
+
+    # Create node_info
+    total_nodes = get_total_nodes(all_sequences)
+    node2idx = {node: idx for idx, node in enumerate(total_nodes)}
+    idx2node = {idx: node for node, idx in node2idx.items()}
+    node_info = {
+        "node2idx": node2idx,
+        "idx2node": idx2node,
+        "total_node": total_nodes
+    }
+    
+    # Splitting data into training and validation sets
+    train_sequences, valid_sequences = train_test_split(all_sequences, train_size=0.8, test_size=0.2, random_state=100)
+
+    # Assuming you have a Dataset class to handle the sequences
+    # Replace 'YourDataset' with the name of your dataset class
+    train_dataset = PangyoDataset(train_sequences, node_info)
+    valid_dataset = PangyoDataset(valid_sequences, node_info)
+
+    # DataLoader setup for training and validation
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+    valid_loader = DataLoader(valid_dataset, batch_size=32)
+    batch = next(iter(train_loader))
+    print(batch)
---
+++ train_Pangyo.py
@@ -0,0 +1,193 @@
+import os
+import numpy as np
+import pickle
+import torch
+import matplotlib.pyplot as plt
+
+from omegaconf import OmegaConf
+from tqdm import tqdm
+from torch.utils.data import Dataset, DataLoader
+from utils import Data, split_validation, AverageMeter
+from data_pangyo import PangyoDataset, set_seed, get_sequences_from_directory, collate_fn, get_total_nodes
+from models import get_model
+from sklearn.model_selection import train_test_split
+
+cfg_fp = "config.yaml"
+cfg = OmegaConf.load(cfg_fp)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+seq_len = 13
+
+# Logging
+# Configuration and initialization
+log_dir = f"./logs/{cfg.model.name}_{seq_len}"
+os.makedirs(log_dir, exist_ok=True)
+
+# Set the seed and load data
+set_seed()
+directory_path = 'C:/Users/HONGGU/.conda/MCterm/datasets/00.processed_csv_file_ssp/13sequence/'
+all_sequences = get_sequences_from_directory(directory_path)
+
+# Create node_info and calculate n_node
+total_nodes = get_total_nodes(all_sequences)
+node2idx = {node: idx for idx, node in enumerate(total_nodes)}
+idx2node = {idx: node for node, idx in node2idx.items()}
+node_info = {
+    "node2idx": node2idx,
+    "idx2node": idx2node,
+    "total_node": total_nodes
+}
+n_node = len(total_nodes)  # The number of unique nodes
+
+# Split and load datasets
+train_sequences, valid_sequences = train_test_split(all_sequences, train_size=0.8, test_size=0.2, random_state=100)
+train_dataset = PangyoDataset(train_sequences, node_info, seq_len=seq_len)
+valid_dataset = PangyoDataset(valid_sequences, node_info, seq_len=seq_len)
+
+train_loader = DataLoader(train_dataset, batch_size=cfg.batchSize, shuffle=True, collate_fn=collate_fn)
+valid_loader = DataLoader(valid_dataset, batch_size=cfg.batchSize, shuffle=False, collate_fn=collate_fn)
+
+# Import Model
+model = get_model(cfg=cfg, num_classes=n_node + 2).to(device)
+
+# Train Model
+# Run training loop
+logs = ["Epoch,Loss,ACC,HITS,HRR\n"]
+val_accs = []
+train_losses = []
+best_acc = 0
+for epoch in range(cfg.epoch):
+    train_loss_meter = AverageMeter()
+    
+    tq = tqdm(train_loader, desc=f"Training [Epoch {epoch+1}/{cfg.epoch}]")
+    model.train()
+    for i, batch in enumerate(tq):
+        alias_inputs, A, items, mask, targets, inputs = batch
+        alias_inputs = alias_inputs.to(device)
+        A = A.to(device)
+        items = items.to(device)
+        mask = mask.to(device)
+        targets = targets.to(device)
+        inputs = inputs.to(device)
+        
+        model.optimizer.zero_grad()
+        # Forward
+        if cfg.model.name == "srgnn":
+            hidden = model(items, A)
+            get = lambda i: hidden[i][alias_inputs[i]]
+            seq_hidden = torch.stack([get(i) for i in torch.arange(len(alias_inputs)).long()])
+            scores = model.compute_scores(seq_hidden, mask)
+        elif cfg.model.name == "lstm":
+            scores = model(inputs)
+        elif cfg.model.name == "transformer":
+            # Transformer forward pass
+            # Assuming the transformer model takes 'inputs' as source and 'targets' as target
+            scores = model(inputs)  # Transformer model forward pass
+            # print(f"Input shape: {inputs.shape}, Scores shape: {scores.shape}, Targets shape: {targets.shape}")
+
+        else:
+            raise NotImplementedError
+
+        loss = model.loss_function(scores, targets)
+        
+        loss.backward()
+        model.optimizer.step()
+        
+        train_loss_meter.update(loss.item(), inputs.size(0))
+        
+        tq.set_postfix(loss=train_loss_meter.avg)
+    train_losses.append(train_loss_meter.avg)
+
+    # Run Validation
+    tq = tqdm(valid_loader, desc=f"Validation [Epoch {epoch+1}/{cfg.epoch}]")
+    model.eval()
+    accuracy = []
+    hits, mrrs = [], []
+    
+    for i, batch in enumerate(tq):
+        alias_inputs, A, items, mask, targets, inputs = batch
+        alias_inputs = alias_inputs.to(device)
+        A = A.to(device)
+        items = items.to(device)
+        mask = mask.to(device)
+        targets = targets.to(device)
+        inputs = inputs.to(device)
+        
+        # Forward
+        if cfg.model.name == "srgnn":
+            hidden = model(items, A)
+            get = lambda i: hidden[i][alias_inputs[i]]
+            seq_hidden = torch.stack([get(i) for i in torch.arange(len(alias_inputs)).long()])
+            scores = model.compute_scores(seq_hidden, mask)
+        elif cfg.model.name == "lstm":
+            scores = model(inputs)
+        elif cfg.model.name == "transformer":
+            # Transformer forward pass
+            # Assuming the transformer model takes 'inputs' as source and 'targets' as target
+            scores = model(inputs)
+        else:
+            raise NotImplementedError
+        
+        # Calculate MRR
+        scores = scores.detach().cpu()
+        targets = targets.detach().cpu()
+
+        k = min(3, scores.size(1))  # Adjust 1 to the dimension of interest
+        sub_scores = scores.topk(k)[1]
+        # sub_scores = scores.topk(20)[1]
+        sub_scores = sub_scores.detach().cpu().numpy()
+        hit, mrr = [], []
+        for score, target, m in zip(sub_scores, targets, mask):
+            hit.append(np.isin(target - 1, score))
+            if len(np.where(score == target - 1)[0]) == 0:
+                mrr.append(0)
+            else:
+                mrr.append(1 / (np.where(score == target - 1)[0][0] + 1))
+        
+        hit = np.mean(hit) * 100
+        mrr = np.mean(mrr) * 100
+        
+        
+        # Calculate Accuracies
+        scores = scores.detach().cpu().numpy()
+        predictions = np.argmax(scores, axis=1)
+        targets = targets.detach().cpu().numpy()
+        acc = np.sum(predictions == targets) / len(targets)
+        acc = acc * 100
+        accuracy.append(acc)
+        tq.set_postfix(acc=acc,
+                       hit=hit,
+                       mrr=mrr)
+
+        hits.append(hit)
+        mrrs.append(mrr)
+        
+    print(f"Validation Results  [Epoch {epoch+1}/{cfg.epoch}]")
+    print(f"Accuracy: {np.mean(accuracy)}")
+    print(f"Hit: {np.mean(hits)}")
+    print(f"MRR: {np.mean(mrrs)}")
+    
+    log_line = f"{epoch+1},{np.mean(accuracy)},{np.mean(hits)},{np.mean(mrrs)}\n"
+    logs.append(log_line)
+    val_accs.append(np.mean(accuracy))
+
+    if np.mean(accuracy) > best_acc:
+        best_acc = np.mean(accuracy)
+        torch.save(model.state_dict(), f"{log_dir}/best_model.pth")
+        print(f"Saved best model with accuracy {best_acc}")
+
+# Save Logs
+with open(f"{log_dir}/logs.txt", "w") as f:
+    f.writelines(logs)
+
+# Save Plot Accuracies
+plt.plot(val_accs)
+plt.title(f"{cfg.model.name} {seq_len} Validation Accuracies")
+plt.xlabel("Epoch")
+plt.ylabel("Accuracy")
+plt.savefig(f"{log_dir}/val_acc.png")
+
+plt.plot(train_losses)
+plt.title(f"{cfg.model.name} {seq_len} Train Losses")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.savefig(f"{log_dir}/val_acc.png")
